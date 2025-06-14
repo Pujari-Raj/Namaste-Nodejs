c097fc5e341441e06db1a31fe9eaf61a092152a8
Pujari Basvaraj <basvarajpujari607@gmail.com>
2025-06-14 13:14:11 +0530

    add: created first post api

--- season-02/devTinder-backend/src/app.js
+++ season-02/devTinder-backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectToDB = require("./config/database");
+const UserModel = require("./models/user");
 
 const app = express();
 
@@ -13,3 +14,22 @@ connectToDB()
   .catch((error) => {
     console.error("Database connection failed", error);
   });
+
+app.post("/signup", async (req, res) => {
+  const user = new UserModel({
+    firstName: 'javagal',
+    lastName: 'shrinath',
+    age: 'forty-five',
+    gender: 'male',
+    emailId: 'king@gmail.com',
+    password: 'KingOfCricket@18'
+  })
+
+  try{
+    await user.save();
+    res.status(200).send('user Added successfully');
+  }catch(error) {
+    console.log("error adding user",error);
+    res.status(400).send("error adding user",error);
+  }
+});
--- season-02/devTinder-backend/src/config/database.js
+++ season-02/devTinder-backend/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectToDB = async () => {
-    await mongoose.connect("mongodb+srv://basvarajpujari607:mggIsHwhae2SY9gn@pujari-basvaraj.kswbskn.mongodb.net/")
+    await mongoose.connect("mongodb+srv://basvarajpujari607:mggIsHwhae2SY9gn@pujari-basvaraj.kswbskn.mongodb.net/test-dev-tinder")
 }
 
 module.exports = connectToDB
